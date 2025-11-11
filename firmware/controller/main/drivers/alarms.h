601302415483a48064968c6c5b0dfd6e207a9e4c
novaeco <novatribolnovea@gmail.com>
2025-11-11 19:18:40 +0100

    Ensure buzzer init before LEDC use and add readiness test

--- firmware/controller/main/drivers/alarms.h
+++ firmware/controller/main/drivers/alarms.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "esp_err.h"
+#include "freertos/FreeRTOS.h"
 #include <stdbool.h>
 
 void alarms_start(void);
 esp_err_t alarms_init(void);
 esp_err_t alarms_set_mute(bool muted);
 bool alarms_get_mute(void);
+bool alarms_wait_ready(TickType_t ticks_to_wait);
